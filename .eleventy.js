200f5257a33e555f3d44352c242cf0ed8008e490
scottpark44 <scottpark44@csu.fullerton.edu>
2021-07-17 19:05:55 -0700

    Test2
    Let's try this

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
+module.exports = {
+    dir: {
+        input: "src",
+        output: "docs"
+    }
+};
+
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
 };
