e4ebcf7360c92f02609c9e244f54610a71b642f5
scottpark44 <scottpark44@csu.fullerton.edu>
2021-07-17 20:04:02 -0700

    Try5

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = {
     dir: {
+        input: "_site",
         output: "docs"
     }
 };
 
-
+/*
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
 };
+*/
